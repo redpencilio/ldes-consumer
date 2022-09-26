2ebedda50b8841205755e9e98a46a00f4af10904
Elena Poelman <elena.poelman@redpencil.io>
2022-09-26 14:04:44 +0200

    use state represenation from actor-init-ldes-client

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,8 +1,6 @@
-import { newEngine, LDESClient } from "@treecg/actor-init-ldes-client";
+import { newEngine, LDESClient, State } from "@treecg/actor-init-ldes-client";
 import * as RDF from "rdf-js";
 import PromiseQueue from "./promise-queue";
-import { extractTimeStamp } from "./utils";
-import { OutgoingHttpHeaders } from 'http';
 export type ConsumerArgs = {
 	endpoint: string;
 	requestHeaders?: {[key: string]: number | string | string[]};
@@ -13,65 +11,44 @@ export type Member = {
 	id: RDF.Term;
 	quads: RDF.Quad[];
 };
-export interface State {
-	timestamp: Date;
-	page: string;
-}
 
 const UPDATE_QUEUE: PromiseQueue<void> = new PromiseQueue<void>();
 
 export default class Consumer {
 	private client: LDESClient;
 	private requestHeaders: {[key: string]: number | string | string[]};
-	private startPage: string;
-	private startTimeStamp?: Date;
-	private currentPage: string;
-	private currentTimeStamp?: Date;
+	private endpoint: string;
+	private initialState: State | undefined;
 
 	constructor({ endpoint, initialState, requestHeaders }: ConsumerArgs) {
-		this.startTimeStamp = initialState?.timestamp;
-		this.currentTimeStamp = initialState?.timestamp;
-		this.startPage = initialState?.page ?? endpoint;
-		this.currentPage = initialState?.page ?? endpoint;
+		this.endpoint = endpoint;
+		this.initialState = initialState;
 		this.requestHeaders = requestHeaders ?? {};
 		this.client = newEngine();
 	}
 
 	async listen(
-		callback: (m: Member, state: State) => void | Promise<void>,
-		onFinish: () => void
+		callback: (m: Member) => void | Promise<void>,
+		onFinish: (s: State) => void
 	) {
-		const stream = this.client.createReadStream(this.startPage, {
+		const stream = this.client.createReadStream(this.endpoint, {
 			representation: "Quads",
 			mimeType: "application/ld+json",
 			requestHeaders: this.requestHeaders,
 			emitMemberOnce: true,
-			fromTime: this.startTimeStamp,
 			disableSynchronization: true,
-		});
-		stream.on("metadata", (metadata: any) => {
-			this.currentPage = metadata.url;
-		});
+		}, this.initialState);
+
 		stream.on("data", async (member: Member) => {
 			try {
-				const timestamp = extractTimeStamp(member);
-				if (
-					!this.currentTimeStamp ||
-					timestamp > this.currentTimeStamp
-				) {
-					this.currentTimeStamp = timestamp;
-				}
 				await UPDATE_QUEUE.push(async () =>
-					callback(member, {
-						timestamp: this.currentTimeStamp!,
-						page: this.currentPage,
-					})
+					callback(member)
 				);
 			} catch (e) {
 				console.error(e);
 			}
 		});
 		stream.on("error", console.error);
-		stream.on("end", onFinish);
+		stream.on("end", () => onFinish(stream.exportState()));
 	}
 }
