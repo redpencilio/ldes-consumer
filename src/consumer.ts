43da3d3dcc1ec7e1a24c2a6b6336bd0b2722dd20
Niels Vandekeybus <progster@gmail.com>
2023-02-10 09:48:51 +0100

    make some ldes options configurable

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,10 +1,20 @@
 import { newEngine, LDESClient, State } from "@treecg/actor-init-ldes-client";
 import * as RDF from "rdf-js";
 import PromiseQueue from "./promise-queue";
+
+export interface LDESOptions {
+  pollingInterval?: number;
+  mimeType?: string;
+  disablePolling?: boolean;
+  dereferenceMembers?: boolean;
+  requestsPerMinute?: number;
+};
+
 export type ConsumerArgs = {
   endpoint: string;
   requestHeaders?: { [key: string]: number | string | string[] };
   initialState?: State;
+  ldesOptions?: LDESOptions;
 };
 
 export type Member = {
@@ -19,27 +29,31 @@ export default class Consumer {
   private requestHeaders: { [key: string]: number | string | string[] };
   private endpoint: string;
   private initialState: State | undefined;
+  private ldesOptions : LDESOptions;
 
-  constructor({ endpoint, initialState, requestHeaders }: ConsumerArgs) {
+  constructor({ endpoint, initialState, requestHeaders, ldesOptions }: ConsumerArgs) {
     this.endpoint = endpoint;
     this.initialState = initialState;
     this.requestHeaders = requestHeaders ?? {};
     this.client = newEngine();
+    this.ldesOptions = ldesOptions;
   }
 
   async listen(
     callback: (m: Member) => void | Promise<void>,
     onFinish: (s: State) => void | Promise<void>
   ) {
+    const defaultOptions = {
+      representation: "Quads",
+      mimeType: "application/ld+json",
+      requestHeaders: this.requestHeaders,
+      emitMemberOnce: true,
+      disableSynchronization: false,
+    };
+    const options = {...defaultOptions, ...this.ldesOptions};
     const stream = this.client.createReadStream(
       this.endpoint,
-      {
-        representation: "Quads",
-        mimeType: "application/ld+json",
-        requestHeaders: this.requestHeaders,
-        emitMemberOnce: true,
-        disableSynchronization: false,
-      },
+      options,
       this.initialState
     );
 
